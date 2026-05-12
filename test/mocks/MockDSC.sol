ef77b3850a33abdee8a0ceb404e40e8daa2e171e
Ariel Poghon <mnangatariel27@gmail.com>
2026-05-12 19:31:53 +0300

    Update MockDSC.sol

--- test/mocks/MockDSC.sol
+++ test/mocks/MockDSC.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
+//mock depo
 contract MockDSC {
     bool private immutable i_mintShouldFail;
     bool private immutable i_transferFromShouldFail;
