b216715b084b6c4b347297b03bf5a9b8595f09ab
Ariel Poghon <mnangatariel27@gmail.com>
2026-05-23 07:15:26 -0400

    Update DeployDSC.s.sol

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-// yoh am scared 
-// prank
 import {Script} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
